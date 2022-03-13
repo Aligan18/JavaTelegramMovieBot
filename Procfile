d4fe25b3cfd199fd99e927e314341a7076c91328
Aligan18 <alisherganiev18@gmail.com>
2022-03-13 09:39:07 +0600

    new add

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: sh target/bin/MyTelegramBot1
+worker: sh target/bin/MyTelegramBot2
--- target/bin/MyTelegramBot2.bat
+++ target/bin/MyTelegramBot2.bat
@@ -76,7 +76,7 @@ goto endInit
 @REM Reaching here means variables are defined and arguments have been captured
 :endInit
 
-%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="MyTelegramBot1" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" src.main.java.classes.java.MyTelegramBot %CMD_LINE_ARGS%
+%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="MyTelegramBot2" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" src.main.java.classes.java.MyTelegramBot %CMD_LINE_ARGS%
 if ERRORLEVEL 1 goto error
 goto end
 
